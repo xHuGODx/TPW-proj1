eecf9e8774d7462704530e46ce71719b52cbf88c
odraude23 <eduardolplopes@ua.pt>
2024-10-23 17:57:12 +0100

    layout

--- webproj/settings.py
+++ webproj/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
+    'bootstrap5',
+    'django_bootstrap_icons',
 ]
 
 MIDDLEWARE = [
