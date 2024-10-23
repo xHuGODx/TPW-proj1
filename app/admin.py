08374614439467b753e7081777abad04feaca011
xHuGODx <hugo.ribeiro04@ua.pt>
2024-10-23 16:33:57 +0100

    update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Product, Comment, Order
+from .models import User, Product, Comment, Order, Message
 
 admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Comment)
 admin.site.register(Order)
+admin.site.register(Message)
 
