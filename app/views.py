c1cc98724017cadbbc3b00b3bbd3ea3ffdf98231
odraude23 <eduardolplopes@ua.pt>
2024-11-08 21:46:52 +0000

    the real end

--- app/views.py
+++ app/views.py
@@ -12,33 +12,6 @@ from django.contrib.auth.hashers import check_password
 
 
 # Create your views here.
-
-def home(request):
-    tparams = {
-        'title': 'Home Page',
-        'year': datetime.now().year,
-    }
-    return render(request, 'index.html', tparams)
-
-
-def contact(request):
-    tparams = {
-        'title': 'Contact',
-        'message': 'Your contact page.',
-        'year': datetime.now().year,
-    }
-    return render(request, 'contact.html', tparams)
-
-
-def about(request):
-    tparams = {
-        'title': 'About',
-        'message': 'Your application description page.',
-        'year': datetime.now().year,
-    }
-    return render(request, 'about.html', tparams)
-    
-
 def index(request):
     # Get unique categories for the filter
     categories = Product.objects.values_list('category', flat=True).distinct()
