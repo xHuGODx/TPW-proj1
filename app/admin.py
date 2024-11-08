f49a8c9e5e253575fc1f3aff936db96e3d2114ad
odraude23 <eduardolplopes@ua.pt>
2024-11-08 19:37:47 +0000

    fix

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import User, Product, Comment, Favourite, Cart, Message, Follower, Order
+from .models import User, Product, Comment, Favorite, Cart, Message, Follower, Order
 
 admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Comment)
 admin.site.register(Message)
-admin.site.register(Favourite)
+admin.site.register(Favorite)
 admin.site.register(Cart)
 admin.site.register(Follower)
 admin.site.register(Order)
